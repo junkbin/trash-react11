0dd1d23ca23d70dd7e29bac1d2a622aa42f0100a
santosh <santosh.ece06@gmail.com>
2018-08-06 13:14:43 +0530

    middleware redux-thunk, axios integration.

--- src/ReduxDemo.js
+++ src/ReduxDemo.js
@@ -3,12 +3,30 @@ import {createStore} from 'redux';
 import {applyMiddleware} from 'redux';
 
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import axios from 'axios';
 
 const reducers = (state, action)=>{
+    switch(action.type){
+        case "FETCH_REQ_START" : {
+            state = {...state, fetching: true};
+            break;
+        }
+
+        case "FETCH_REQ_ERROR" : {
+            state = {...state, error:action.payload};
+            break;
+        }
+
+        case "FETCH_REQ_DONE" : {
+            state = {...state, fetching:false, fetched:true, data:action.payload};
+            break;
+        }
+    }
     return state;
 };
 
-const refMiddleWare = applyMiddleware(logger);
+const refMiddleWare = applyMiddleware(thunk, logger);
 const store = createStore(reducers, refMiddleWare);
 
 store.subscribe(()=>{
@@ -16,5 +34,18 @@ store.subscribe(()=>{
 });
 
 store.dispatch({"type":"INC", "payload":0});
+
+store.dispatch((dispatch)=>{
+    dispatch({type:'FETCH_REQ_START'});
+
+    let url = "https://jsonplaceholder.typicode.com/posts";
+    let mpromise = axios.get(url);
+    mpromise.then((data)=>{
+
+        dispatch({type:"FETCH_REQ_DONE", payload:data.data});
+    }).catch((err)=>{
+        dispatch({"type":"FETCH_REQ_ERROR", payload:err});
+    });
+});
 
 export default store;
